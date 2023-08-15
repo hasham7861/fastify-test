df21f71aa1beef9f5601ef8753d8c04ffba14891
user <user@users-MacBook-Pro.local>
2023-08-15 19:40:38 -0400

    its beautiful

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,23 @@ import fastify from 'fastify'
 
 const server = fastify()
 
+
+interface IQuerystring {
+  username: string;
+  password: string;
+}
+
+interface IHeaders {
+  'h-Custom': string;
+}
+
+interface IReply {
+  200: { success: boolean };
+  302: { url: string };
+  '4xx': { error: string };
+}
+
+
 server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
@@ -12,4 +29,22 @@ server.listen({ port: 8080 }, (err, address) => {
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
+})
+
+server.get<{
+  Querystring: IQuerystring,
+  Headers: IHeaders,
+  Reply: IReply
+}>('/auth', (request, reply) => {
+  const { username, password } = request.query
+  const customerHeader = request.headers['h-Custom']
+
+  console.log({
+    username,
+    password,
+    customerHeader
+  })
+
+  reply.code(200).send({ success: true });
+
 })
